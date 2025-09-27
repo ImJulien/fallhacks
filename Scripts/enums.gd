147935bb61b43c3d64c32bb8a2fc2bf4bf096377
Kona <mfranchuk97@gmail.com>
2025-09-27 10:20:23 -0700

    enums

---
+++ Scripts/enums.gd
@@ -0,0 +1,10 @@
+extends Node
+
+enum AggressionLevel {
+	PASSIVE, # At least 1 correct
+	IRRITATED, # At least 20% correct
+	AGITATED, # At least 40% correct
+	AGGRESSIVE, # At least 60% correct
+	VIOLENT, # At least 80% correct
+	HOSTILE # Must be correct
+}
