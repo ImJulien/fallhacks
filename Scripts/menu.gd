ec457c6818205b6992390f8458813820bcc25fd5
ImJulien <64613853+ImJulien@users.noreply.github.com>
2025-09-27 12:16:06 -0700

    ha

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-var windows = ['news', 'play', 'options']
-@onready var play_window = $Windows/Margins/PlayWindow
 @onready var options_window = $Windows/Margins/OptionsWindow
 @onready var changelog_window = $Windows/Margins/ChangelogWindow
 
@@ -9,26 +7,17 @@ var windows = ['news', 'play', 'options']
 @onready var controls_tab = $Windows/Margins/OptionsWindow/Controls
 @onready var display_tab = $Windows/Margins/OptionsWindow/Display
 
-@export var selected_window = windows[0]
-
 func _on_play_pressed():
-	play_window.visible = true
-	options_window.visible = false
-	changelog_window.visible = false
-	selected_window = windows[1]
-	pass
+	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
 
 func _on_options_pressed():
-	play_window.visible = false
 	options_window.visible = true
 	changelog_window.visible = false
-	selected_window = windows[2]
 	pass
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
-
 func _on_general_pressed():
 	general_tab.visible = true
 	controls_tab.visible = false
