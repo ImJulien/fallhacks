1fcfcf4936247c0e717d3cfb08a0a5fd618cb447
Kona <mfranchuk97@gmail.com>
2025-09-27 14:57:18 -0700

    Update main_game.gd

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -14,12 +14,3 @@ func _process(_delta):
 		var tween_down := create_tween()
 		tween_down.tween_property(ui_container, "position:y", 250, 0.3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 		tween_down.play()
-
-func calc_perf(ingredients: Array[Ingredient]):
-	# calculate the final perfection of the dish
-	# later expand to compare ingredients
-	var total_doneness: float = 0
-	for ingredient in ingredients:
-		total_doneness += ingredient.doneness
-	var avg_doneness: float = total_doneness / ingredients.size()
-	return avg_doneness
