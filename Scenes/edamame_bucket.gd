d0e26065cb6a3d65d3e4766fa249cde817b0971e
ImJulien <64613853+ImJulien@users.noreply.github.com>
2025-09-27 17:29:07 -0700

    ddkjkkdjdj

--- Scenes/edamame_bucket.gd
+++ Scenes/edamame_bucket.gd
@@ -9,9 +9,15 @@ func _ready():
 func spawn_item():
 	var item_base = preload("res://Scenes/tomato.tscn")
 	var new_edemame = item_base.instantiate()
+	var item_area: Area2D = new_edemame.get_node_or_null("Area2D")
+	if item_area:
+		item_area.monitorable = true
+		item_area.set_collision_layer_value(1, true)
+	else:
+		push_warning("Spawned item has no Area2D child named 'Area2D'")
 	var edemame_sprite: Sprite2D = new_edemame.get_node('Sprite2D')
 	edemame_sprite.texture = load('res://Assets/Sprites/Foodge/Edamame.png')
-	new_edemame.name = "NewTomato"
+	new_edemame.name = "Edemame"
 	new_edemame.global_position = get_viewport().get_mouse_position()
 	parent_node.add_child(new_edemame)
 	new_edemame.start_drag(get_viewport().get_mouse_position())
